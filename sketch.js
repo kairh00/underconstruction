ff782a3f838f3a65350f271ab9681d5c6dfc9509
kairh00 <65600128+kairh00@users.noreply.github.com>
2025-05-01 17:30:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let r=200; let g=0;
 
 
 function setup() { 
-  createCanvas(windowWidth,windowWidth/2.1 + 200);
+  createCanvas(windowWidth,windowHeight/1.5 + 200);
   frameRate(60);
   count=0;
    
@@ -17,22 +17,20 @@ function draw() {
   stroke(0);
   strokeWeight(2);
   strokeCap(ROUND);
-rect(0,0,width,windowWidth/2.1);
+rect(0,0,width,windowHeight/1.5);
  textX += moveX;
   textY += moveY;
   
 
-square(windowWidth/2-125,windowWidth/2.1+50,20);
+square(windowWidth/2-125,windowHeight/1.5+50,20);
   fill(200,200,220);
   
-    square(windowWidth/2-75,windowWidth/2.1+50,20);
-     square(windowWidth/2-25,windowWidth/2.1+50,20);
-  square(windowWidth/2+25,windowWidth/2.1+50,20);
+    square(windowWidth/2-75,windowHeight/1.5.1+50,20);
+     square(windowWidth/2-25,windowHeight/1.5.1+50,20);
+  square(windowWidth/2+25,windowHeight/1.5+50,20);
   
-   square(windowWidth/2+75,windowWidth/2.1+50,20);
-   square(windowWidth/2+125,windowWidth/2.1+50,20);
-  
-  
+   square(windowWidth/2+75,windowHeight/1.5.1+50,20);
+   square(windowWidth/2+125,windowHeight/1.5.1+50,20); 
    
 
   textFont('Ubuntu Mono');
@@ -48,7 +46,7 @@ square(windowWidth/2-125,windowWidth/2.1+50,20);
  if (textX > width-180 || textX < 180){
    moveX *= -1; r=random(50,200); g=random(1,100);
  }
-  if (textY > windowWidth/2.1-25 || textY < 20){
+  if (textY > windowHeight/1.5-25 || textY < 20){
    moveY *= -1;r=random(50,200); g=random(1,100);
  }
   
